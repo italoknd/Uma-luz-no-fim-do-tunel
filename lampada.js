d7ffa42e2496f3f1850c92d200814698bd593da3
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2021-11-02 19:57:41 -0400

    ajustes de background incrementados

--- lampada.js
+++ lampada.js
@@ -10,22 +10,25 @@ broke.addEventListener("click", quebrar)
 function ligar(){
   
   if(Number(value) == 1){
-
+    alert("Impossível fazer isso, a lampada esta quebrada!")
   }else{
     lamp.src = "./img/ligada.png"
+    document.body.style.background = '#F3B700'
   }
 }
 
 function desligar(){
 
   if(Number(value) == 1){
-
+    alert("Impossível fazer isso, a lampada esta quebrada!")
   }else{
     lamp.src = "./img/desligada.png"
+    document.body.style.background = '#0D1B2A'
   }
 }
 
 function quebrar(){
   lamp.src = "./img/quebrada.png"
   value = 1
+  document.body.style.background = '#071013'
 }
